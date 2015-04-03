5c3a1e711164be50bf2cd0f69a6fd08866c3ac19
Sarah Harvey <sharvey@metaether.net>
2015-04-03 14:34:34 -0400

    Fix a float bug

--- main/readability_graph.py
+++ main/readability_graph.py
@@ -10,34 +10,34 @@ if __name__ == '__main__':
     result = corpus.run_sql('SELECT * FROM comment_feature_read LIMIT 100', None)
     print('Got results')
 
-    values = [ v['ari'] for v in result ]
+    values = [ float(v['ari']) for v in result ]
     graph.hist('data/ari_hist', values, 'ARI', 'Frequency',
                'Frequency of ARI values')
 
-    values = [ v['flesch_reading_ease'] for v in result ]
+    values = [ float(v['flesch_reading_ease']) for v in result ]
     graph.hist('data/flesch_reading_ease_hist', values, 'Flesch Reading Ease', 'Frequency',
                'Frequency of Flesch Reading Ease values')
 
-    values = [ v['flesch_kincaid_grade_level'] for v in result ]
+    values = [ float(v['flesch_kincaid_grade_level']) for v in result ]
     graph.hist('data/flesch_kincaid_grade_level_hist', values, 'Flesch Kincaid Grade Level', 'Frequency',
                'Frequency of Flesch Kincaid Grade Level values')
 
-    values = [ v['gunning_fog_index'] for v in result ]
+    values = [ float(v['gunning_fog_index']) for v in result ]
     graph.hist('data/gunning_fog_index_hist', values, 'Gunning Fog Index', 'Frequency',
                'Frequency of Gunning Fog Index values')
 
-    values = [ v['smog_index'] for v in result ]
+    values = [ float(v['smog_index']) for v in result ]
     graph.hist('data/smog_index_hist', values, 'Smog Index', 'Frequency',
                'Frequency of Smog Index values')
 
-    values = [ v['coleman_liau_index'] for v in result ]
+    values = [ float(v['coleman_liau_index']) for v in result ]
     graph.hist('data/coleman_liau_index_hist', values, 'Coleman Liau Index', 'Frequency',
                'Frequency of Coleman Liau Index values')
 
-    values = [ v['lix'] for v in result ]
+    values = [ float(v['lix']) for v in result ]
     graph.hist('data/lix_hist', values, 'LIX', 'Frequency',
                'Frequency of LIX values')
 
-    values = [ v['rix'] for v in result ]
+    values = [ float(v['rix']) for v in result ]
     graph.hist('data/rix_hist', values, 'RIX', 'Frequency',
                'Frequency of RIX values')
