08fc45be6746339ced9c0703c115d7a2b2abc3ed
Sarah Harvey <sharvey@metaether.net>
2015-04-03 18:00:09 +0000

    Fix run_sql call in graph generation

--- main/readability_graph.py
+++ main/readability_graph.py
@@ -6,8 +6,8 @@ import cred
 if __name__ == '__main__':
     corpus = RedditMySQLCorpus()
     corpus.setup(**(cred.kwargs))
-    corpus.create()
 
-    result = corpus.run_sql('SELECT ari FROM comment_feature_read')
+    result = corpus.run_sql('SELECT ari FROM comment_feature_read', None)
+    print('Got results')
     values = [ result[i]['ari'] for i in result ]
     graph.hist('data/ari_hist', values, 'ARI', 'Frequency', 'Frequency of ARI values')
