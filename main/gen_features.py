8bfb5e3f7f1166dfe7da19c4a3c946942b3105fc
Sarah Harvey <sharvey@metaether.net>
2015-04-09 13:37:52 -0400

    Some fixes

--- main/gen_features.py
+++ main/gen_features.py
@@ -46,9 +46,11 @@ if __name__ == '__main__':
     print('set up pool')
 
     chunk = 200
-    j = 0
     feature_set = set()
     for reddit in ['worldnews', 'quantum', 'netsec', 'uwaterloo', 'gaming', 'news', 'AskReddit']:
+        j = 0
+        if reddit == 'worldnews':
+            j = 204000
         while True:
             print('j=%d' % j)
             rows = corpus.run_sql('SELECT `body` AS `text` FROM `comment` '
@@ -58,7 +60,7 @@ if __name__ == '__main__':
                                   'LIMIT %d, %d' % (reddit, j, chunk), None)
             if len(rows) == 0:
                 break
-            it = pool.imap_unordered(gen_feature, rows)
+            it = pool.imap_unordered(gen_feature, rows, 10)
             new_feature_set = set()
             while True:
                 try:
@@ -73,14 +75,13 @@ if __name__ == '__main__':
             for f in list(new_feature_set):
                 fp.write('\'%s\',\'%s\'\n' % (f[0], f[1].encode('utf-8').replace('\\', '\\\\').replace('\n', '\\n').replace('\'', '\\\'')))
             fp.close()
-            corpus.run_sql('LOCK TABLES `feature_map_test` WRITE', None)
             query = 'LOAD DATA INFILE \'/tmp/sharvey_features\' IGNORE INTO TABLE `feature_map_test` '\
                            'FIELDS TERMINATED BY \',\' OPTIONALLY ENCLOSED BY \'\\\'\' '\
                            'LINES TERMINATED BY \'\\n\' ' \
                            '(`type`, `feature`)'
             print(query)
             corpus.run_sql(query, None)
-            corpus.run_sql('UNLOCK TABLES', None)
-            #pprint.pprint(len(new_feature_set))
             feature_set = feature_set.union(new_feature_set)
             j += chunk
+        if j % 5000 == 0:
+            feature_set = set()
