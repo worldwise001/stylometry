710c915922a8350300e65f02e57dfbd211d052cc
Sarah Harvey <sharvey@metaether.net>
2015-04-08 20:49:07 -0400

    Add fix for urls

--- main/gen_features.py
+++ main/gen_features.py
@@ -8,29 +8,32 @@ from feature import lexical
 import cred
 
 import pprint
+import re
 
 def gen_feature(atuple):
+    text = re.sub(r'https?://([a-zA-Z0-9\.\-_]+)[\w\-\._~:/\?#@!\$&\'\*\+,;=%%]*',
+                  '\\1', atuple['text'], flags=re.MULTILINE)
     aset = set()
-    bngram = ngram.get_byte_ngrams(atuple['text'])
+    bngram = ngram.get_byte_ngrams(text)
     for n in bngram['ngram_byte']:
         for k in bngram['ngram_byte'][n]:
             aset.add(('nb%d' % n, k))
     for n in bngram['ngram_byte_cs']:
         for k in bngram['ngram_byte_cs'][n]:
             aset.add(('nbcs%d' % n, k))
-    wngram = ngram.get_word_ngrams(atuple['text'])
+    wngram = ngram.get_word_ngrams(text)
     for n in wngram['ngram_word']:
         for k in wngram['ngram_word'][n]:
             aset.add(('nw%d' % n, ' '.join(k)))
     for n in wngram['ngram_word_clean']:
         for k in wngram['ngram_word_clean'][n]:
             aset.add(('nwc%d' % n, ' '.join(k)))
-    words, clean_words = ngram.get_words(atuple['text'])
+    words, clean_words = ngram.get_words(text)
     for word in words:
         aset.add(('w', word))
     for word in clean_words:
         aset.add(('cw', word))
-    lex = lexical.get_symbol_dist(atuple['text'])
+    lex = lexical.get_symbol_dist(text)
     for k in lex['lex']:
         aset.add(('l', k))
     #pprint.pprint(aset)
@@ -46,7 +49,7 @@ if __name__ == '__main__':
     chunk = 100
     j = 0
     feature_set = set()
-    for reddit in ['worldnews', 'news', 'quantum', 'netsec', 'uwaterloo', 'gaming', 'askreddit ']:
+    for reddit in ['worldnews', 'news', 'quantum', 'netsec', 'uwaterloo', 'gaming']:
         while True:
             print(j)
             rows = corpus.run_sql('SELECT `body` AS `text` FROM `comment` '
